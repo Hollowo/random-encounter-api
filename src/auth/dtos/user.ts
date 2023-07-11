08297783ab8401583565d240af7f3ebbc4b4943f
VITORPASSOS <vitoreugenio02@gmail.com>
2023-07-11 17:46:43 -0300

    user

--- src/auth/dtos/user.ts
+++ src/auth/dtos/user.ts
@@ -1,7 +1,11 @@
+import { Role } from '@prisma/client';
 import { IsNotEmpty, Length } from 'class-validator';
 
 export class CreateUserBody {
 	@IsNotEmpty()
 	@Length(3, 100)
 	name: string;
+
+	@IsNotEmpty()
+	role: Role;
 }
